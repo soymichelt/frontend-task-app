f2ffe09718db647fcc1c58b89acd93d77646232c
Soymichel Dev <mtraatabladaa94@gmail.com>
2024-10-22 14:23:13 -0600

    feat: move header and footer to layout components

--- src/app/shared/layouts/auth/auth.component.ts
+++ src/app/shared/layouts/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-import { FooterComponent } from '../../components/footer/footer.component';
-import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-auth-layout',
