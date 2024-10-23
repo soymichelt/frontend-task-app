5e6c92ea156ca216498c6e885f787942d0bdac5b
Soymichel Dev <mtraatabladaa94@gmail.com>
2024-10-22 22:53:50 -0600

    feat: add firebase config

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'tasksapp-dev',
+    appId: '1:222317116895:web:84565daf73b3a0c9f9986a',
+    storageBucket: 'tasksapp-dev.appspot.com',
+    apiKey: 'AIzaSyCtFkuaLQuc8ZB-mRv2S9WlYFOfRcAQBcs',
+    authDomain: 'tasksapp-dev.firebaseapp.com',
+    messagingSenderId: '222317116895',
+    measurementId: 'G-1TW2LPLHRP',
+  },
+};
+
