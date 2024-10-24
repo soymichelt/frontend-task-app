2f34568c945d0d71ef20b07dc85015834a2f233b
Soymichel Dev <mtraatabladaa94@gmail.com>
2024-10-24 14:03:38 -0600

    fix: correcting bug in tasks model

--- src/app/core/models/tasks/tasks.model.ts
+++ src/app/core/models/tasks/tasks.model.ts
@@ -2,6 +2,6 @@ import { SuccessResultModel } from '../result/success-result.model';
 import { TaskModel } from './task.model';
 
 export type TasksResultModel = Omit<SuccessResultModel, 'data'> & {
-  data: TaskModel[];
+  body: TaskModel[];
 };
 
